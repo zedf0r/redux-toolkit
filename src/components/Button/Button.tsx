c4dacfd3513973c65be14bfd0e05cacf44d01528
Leonid <zemlyanykh@bk.ru>
2025-07-22 15:00:53 +0500

    Udpate project

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,4 @@
-// import { useDispatch } from "react-redux";
 import style from "./Button.module.css";
-// import type { TypeFilmParams } from "../../types";
-// import type { AppDispatch } from "../../services/store";
-// import type { ActionCreatorWithPayload } from "@reduxjs/toolkit";
-
-// type TypeButtonActions = {
-//   action: ActionCreatorWithPayload<TypeFilmParams>;
-//   film: TypeFilmParams;
-//   children: React.ReactNode;
-// };
 
 type TypeButtonProps = {
   onClick: (event: React.MouseEvent) => void;
@@ -16,7 +6,6 @@ type TypeButtonProps = {
 };
 
 export const Button = ({ onClick, children }: TypeButtonProps) => {
-  //   const dispatch: AppDispatch = useDispatch();
   return (
     <button type="button" className={style.button} onClick={onClick}>
       {children}
