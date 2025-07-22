3718b57e9aeb4c3244ed648ab35e1f050637f803
Leonid <zemlyanykh@bk.ru>
2025-07-22 15:18:25 +0500

    Update project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import { Layout } from "./components/layouts/Layot";
+import { CardPage, FavoritePage, SearchPage } from "./pages";
 
 function App() {
-  return <Layout />;
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<SearchPage />} />
+        <Route path="/:id" element={<CardPage />} />
+        <Route path="/favorite" element={<FavoritePage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
--- src/components/layouts/Layot.tsx
+++ src/components/layouts/Layot.tsx
@@ -1,16 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Header } from "../Header/Header";
-import { CardPage, FavoritePage, SearchPage } from "../../pages";
 
 export const Layout = () => {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<SearchPage />} />
-        <Route path="/:id" element={<CardPage />} />
-        <Route path="/favorite" element={<FavoritePage />} />
-      </Routes>
+      <Outlet />
     </>
   );
 };
